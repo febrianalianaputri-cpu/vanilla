7d8bf10d4a5d56a6ae5644e91d53fdd9f22bbd33
febrianalianaputri-cpu <febrianalianaputri@gmail.com>
2026-06-12 12:13:50 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,60 +1,57 @@
-/* 
 
-Vanilla Template
+(function ($) {
+    "use strict";
 
-https://templatemo.com/tm-526-vanilla
+    
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
 
-*/
+    $('.validate-form').on('submit',function(){
+        var check = true;
 
-jQuery(document).ready(function($) {
+        for(var i=0; i<input.length; i++) {
+            if(validate(input[i]) == false){
+                showValidate(input[i]);
+                check=false;
+            }
+        }
 
-	'use strict';
-
-    var top_header = $('.parallax-content');
-    top_header.css({'background-position':'center center'}); // better use CSS
-
-    $(window).scroll(function () {
-    var st = $(this).scrollTop();
-    top_header.css({'background-position':'center calc(50% + '+(st*.5)+'px)'});
+        return check;
     });
 
 
-    $('body').scrollspy({ 
-        target: '.fixed-side-navbar',
-        offset: 200
-    });
-      
-      // smoothscroll on sidenav click
-
-    $('.tabgroup > div').hide();
-        $('.tabgroup > div:first-of-type').show();
-        $('.tabs a').click(function(e){
-          e.preventDefault();
-            var $this = $(this),
-            tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
-            others = $this.closest('li').siblings().children('a'),
-            target = $this.attr('href');
-        others.removeClass('active');
-        $this.addClass('active');
-        $(tabgroup).children('div').hide();
-        $(target).show();
-      
-    })
-
-    var owl = $("#owl-testimonials");
-
-      owl.owlCarousel({
-        
-        pagination : true,
-        paginationNumbers: false,
-        autoPlay: 6000, //Set AutoPlay to 3 seconds
-        items : 3, //10 items above 1000px browser width
-        itemsDesktop : [1000,3], //5 items between 1000px and 901px
-        itemsDesktopSmall : [900,2], // betweem 900px and 601px
-        itemsTablet: [600,1], //2 items between 600 and 0
-        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-        
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
     });
 
+    function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+    }
 
-});
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
